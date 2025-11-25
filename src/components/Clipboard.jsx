5b3abda8c245c0243b08b2c9e4c4bdb6275d1956
LI VALERIY ANDREYEVICH <astramac@Mac.bbrouter>
2025-11-25 21:48:08 +0500

    clipboard textcolor fix

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -1,11 +1,12 @@
 import { Clipboard, IconButton } from "@chakra-ui/react"
-
+import { useTheme } from '@/store'
 
 export default function CopyButton({content}){
+  const { theme } = useTheme();
   return (
     <Clipboard.Root value={content} border="none" >
       <Clipboard.Trigger  asChild bgColor="transparent" border="none" boxShadow="none">
-        <IconButton variant="surface" transform="scale(1.2)" color="white" size="xs">
+        <IconButton variant="surface" transform="scale(1.2)" color={theme.textColor} size="xs">
           <Clipboard.Indicator />
         </IconButton>
       </Clipboard.Trigger>
